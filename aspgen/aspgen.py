50c2eaf16870345be8d172157971cc42d7f66ab4
TheOneHyer <theonehyer@gmail.com>
2016-06-18 00:14:05 +0800

    Added basic_stats
    Added basic_stats function to aspgen to
    calculate password randomness.

--- aspgen/aspgen.py
+++ aspgen/aspgen.py
@@ -8,6 +8,7 @@ from __future__ import print_function
 """
 
 import argparse
+import math
 import os
 import random
 import sys
@@ -21,25 +22,57 @@ __status__ = 'Alpha'
 __version__ = '0.0.2'
 
 
+def basic_stats(password):
+    """Produce basic stats on a password
+
+    Args:
+        password (str): password to analyze
+
+    Returns:
+        int, float: number of password combinations and entropy of password
+    """
+
+    # Get character sets
+    pass_set = set(password)
+    lower_letters = set(password_characters(all=False, lower_letters=True))
+    upper_letters = set(password_characters(all=False, upper_letters=True))
+    special_chars = set(password_characters(all=False, special_chars=True))
+
+    possible_chars = 0
+
+    # Detect characters and combine sets
+    if len(pass_set.intersection(lower_letters)) != 0:
+        possible_chars += len(lower_letters)
+    if len(pass_set.intersection(upper_letters)) != 0:
+        possible_chars += len(upper_letters)
+    if len(pass_set.intersection(special_chars)) != 0:
+        possible_chars += len(special_chars)
+
+    combinations = possible_chars ** len(password)
+    entropy = math.log(combinations, 2)
+
+    return combinations, entropy
+
+
 def password_characters(all=True, lower_letters=False, upper_letters=False,
-                        special_characters=False):
+                        special_chars=False):
     """Create a list of possible characters for a password
 
     Args:
         all (bool): same as setting lower_letters, upper_letters, and
-                    special_characters all to True
+                    special_chars all to True
 
         lower_letters (bool): add lower-case letters to character list
 
         upper_letters (bool): add upper-case letters to character list
 
-        special_characters (bool): add special characters to character list
+        special_chars (bool): add special characters to character list
 
     Returns:
         list: List of str characters for password
 
     Example:
-        >>> password_characters(all=False, special_characters=True)
+        >>> password_characters(all=False, special_chars=True)
         [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-',
         '.', ':', ';', '<', '=', '>', '?', '[', '\\', ']', '^', '_', '{', '|',
         '}']
@@ -48,16 +81,16 @@ def password_characters(all=True, lower_letters=False, upper_letters=False,
     characters = []
 
     if lower_letters or all:
-        characters += [chr(char) for char in range(97, 122)]
+        characters += [chr(char) for char in range(97, 123)]
 
     if upper_letters or all:
-        characters += [chr(char) for char in range(65, 90)]
+        characters += [chr(char) for char in range(65, 91)]
 
-    if special_characters or all:
-        characters += [chr(char) for char in range(32, 47)]
-        characters += [chr(char) for char in range(58, 64)]
-        characters += [chr(char) for char in range(91, 96)]
-        characters += [chr(char) for char in range(123, 126)]
+    if special_chars or all:
+        characters += [chr(char) for char in range(32, 48)]
+        characters += [chr(char) for char in range(58, 65)]
+        characters += [chr(char) for char in range(91, 97)]
+        characters += [chr(char) for char in range(123, 127)]
 
     return characters
 
@@ -75,6 +108,9 @@ def output(message, width=79):
         a long line
     """
 
+    # Represent brackets literally
+    message = message.replace('{', '{{')
+    message = message.replace('}', '}}')
     print('{0}'.join(wrap(message, width)).format(os.linesep))
 
 
@@ -94,12 +130,18 @@ def main(args):
         chars = password_characters(all=args.all,
                                     lower_letters=args.lower_letters,
                                     upper_letters=args.upper_letters,
-                                    special_characters=args.special_characters)
+                                    special_chars=args.special_characters)
         max_char = len(chars) - 1
 
-        for i in range(args.length):
+        for i in range(0, args.length):
             password.append(chars[random.SystemRandom().randint(0, max_char)])
-        output(''.join(password))
+        password = ''.join(password)
+        output('Password: {0}'.format(password))
+
+        if args.stats:
+            combinations, entropy = basic_stats(password)
+            output('Password Combinations: {0}'.format(combinations))
+            print('Password Entropy: {0}'.format(entropy))
 
     if args.tool == 'analyzer':
         print('Tool not ready yet')
@@ -120,6 +162,9 @@ if __name__ == '__main__':
                            default=True,
                            action='store_true',
                            help='permit all characters in password [Default]')
+    generator.add_argument('-d', '--dictionary',
+                           action='store_true',
+                           help='generate dictionary based password')
     generator.add_argument('-l', '--length',
                            default=12,
                            type=int,
@@ -137,6 +182,9 @@ if __name__ == '__main__':
     generator.add_argument('-s', '--special_characters',
                            action='store_true',
                            help='permit special characters in password')
+    generator.add_argument('-t', '--stats',
+                           action='store_true',
+                           help='print basic stats on password')
     generator.add_argument('-u', '--upper_letters',
                            action='store_true',
                            help='permit uppercase letters in password')
